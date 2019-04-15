18ea55464dd28f84359fa51a5e7272a535aa3e6f
feltzshae <shae@notics.io>
2019-04-15 07:56:21 -0500

    Changed Variable Names

--- src/Subtract.java
+++ src/Subtract.java
@@ -1,9 +1,9 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
 
 @WebServlet(name = "Subtract")
 public class Subtract extends HttpServlet {
@@ -13,12 +13,12 @@ public class Subtract extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        int a = Integer.parseInt(request.getParameter("num3"));
-        int b = Integer.parseInt(request.getParameter("num4"));
-        int m = a - b;
+        int i = Integer.parseInt(request.getParameter("num3"));
+        int j = Integer.parseInt(request.getParameter("num4"));
+        int k = i - j;
 
         response.getWriter();
-        response.getWriter().println("Result is " + m);
+        response.getWriter().println("Result is " + k);
 
     }
 }
